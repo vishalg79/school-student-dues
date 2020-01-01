afe0502796cdbaee843c846efc7c72bb4e0e6b8f
Vishal Gupta <vishalg79@gmail.com>
2020-01-01 13:05:17 -0500

    add all dependencies

--- add_dues.py
+++ add_dues.py
@@ -1,19 +1,23 @@
 import boto3, printCreds as creds
 
-creds.printCurrCreds()
+# creds.printCurrCreds()
 
-ddb = boto3.resource('dynamodb')
-# assert isinstance(ddb, boto3.dynamodb)
+def add_student_dues(event, context) :
+    ddb = boto3.resource('dynamodb')
+    # assert isinstance(ddb, boto3.dynamodb)
 
-students = ddb.Table('Students')
-# assert isinstance(students, boto3.dynamodb.table.TableResource)
+    students = ddb.Table('Students')
+    # assert isinstance(students, boto3.dynamodb.table.TableResource)
 
-print(students.creation_date_time)
+    # print(students.creation_date_time)
 
-students.put_item(
-    Item = {
-        'StudentId' : 1,
-        'ItemPurchased' : 'EDC',
-        'Amount' : 10,
-    }
-)
+    students.put_item(
+        Item = {
+            'StudentId' : 1,
+            'ItemPurchased' : 'EDC',
+            'Amount' : 10,
+        }
+    )
+
+    # assert isinstance(event, dict)
+
